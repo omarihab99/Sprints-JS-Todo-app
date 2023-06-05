138db4c8808aa91a92dd1c8b2e479484c0b63d1f
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-06-05 16:15:02 +0300

    Added Task class and refractored validate and sort

--- index.js
+++ index.js
@@ -1,71 +1,38 @@
 "use strict";
-function createTodo([...props]) {
-  let [id, taskName, priority] = props;
-  let todo = {
-    id: id,
-    taskName: taskName,
-    priority: priority,
-  };
-  return todo;
-}
 
-// Create a recursive binary search function that takes a sorted array and a value and returns the index of the value in the array.
-// Note: the array consists of objects and search will be over the priority property of the objects.
-function binarySearch(array, min, max, value) {
-  if (max >= min) {
-    let mid = Math.floor((min + max) / 2);
-    if (array[mid].priority <= value) {
-      return binarySearch(array, mid+1, max, value);
-    } else if (array[mid].priority > value) {
-      return binarySearch(array, min, mid-1, value);
-    }
+export class Task {
+  _taskName;
+  _priority;
+  _ID;
+  _status = "pending";
+  constructor(taskName, priority, ID) {
+    this.taskName = taskName;
+    this.priority = priority;
+    this._ID = ID;
   }
-  return max;
-}
-// Add a new todo to the list.
-export function addTodoToList([...props], list) {
-  let todo = createTodo([...props]);
-  if(list.length === 0){
-    list.push(todo);
-    return list;
+  get status() {
+    return this._status;
   }
-  let index = binarySearch(list, 0, list.length - 1, todo.priority);
-  list.splice(index+1, 0, todo);
-  return list;
+  set status(status) {
+    this._status = status;
+  }
+  get taskName() {
+    return this._taskName;
+  }
+  set taskName(taskName) {
+    this._taskName = taskName;
+  }
+  get priority() {
+    return this._priority;
+  }
+  set priority(priority) {
+    this._priority = priority;
+  }
+  get ID() {
+    return this._ID;
+  }
+  set ID(ID) {
+    this._ID = ID;
+  }
+  
 }
-
-// Remove a todo from the list.
-export function removeTodoFromList(id, list) {
-    for(let todo of list){
-        if(todo.id === id){
-            list.splice(list.indexOf(todo), 1);
-            return list;
-        }
-    }
-} 
-
-// Edit a todo.
-export function editTodo(id, taskName, priority, list) {
-    for(let todo of list){
-        if(todo.id === id){
-            todo.taskName = taskName ? taskName : todo.taskName;
-            todo.priority = priority ? priority : todo.priority;
-            if(priority) list.sort((a, b) => a.priority - b.priority);
-            return list;
-        }
-    }
-}
-
-// A function test the functionality of the above functions
-// function test() {
-//     let list = [];
-//     list = addTodoToList([1, "task1", 1], list);
-//     console.log(list);
-//     list = addTodoToList([2, "task2", 2], list);
-//     list = addTodoToList([3, "task3", 3], list);
-//     console.log(list);
-//     list = editTodo(1, "task4", 4, list);
-//     console.log(list);
-//     list = removeTodoFromList(1, list);
-//     console.log(list);
-// }
--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-import { addTodoToList, removeTodoFromList, editTodo } from "./index.js";
+import {Task} from "./index.js";
 
-const validateEdit = (taskName, priority) => {
+const validate = (taskName, priority, addFlag) => {
   const priorityPattern = /^[1-9]*$/;
   const taskNamePattern = /[a-zA-Z0-9]/;
-  if (
-    (taskName.trim() &&
-      taskName.length > 0 &&
-      taskNamePattern.test(taskName)) ||
-    (priority.trim() && priority.length > 0 && priorityPattern.test(priority))
-  ) {
-    return true;
-  }
-  return false;
-};
-const validate = (taskName, priority) => {
-  const priorityPattern = /^[1-9]*$/;
-  const taskNamePattern = /[a-zA-Z0-9]/;
-  if (
-    taskName.trim() &&
-    priorityPattern.test(priority) &&
-    taskNamePattern.test(taskName) &&
-    taskName.length > 0 &&
-    priority.length > 0
-  ) {
-    return true;
-  }
-  return false;
-};
-const createTodoItem = (taskName, priority, list, ID) => {
+  let validationState;  
+  switch (addFlag) {
+      case true:
+        if (
+          taskName.trim() &&
+          priorityPattern.test(priority) &&
+          taskNamePattern.test(taskName) &&
+          taskName.length > 0 &&
+          priority.length > 0
+        ) {
+          validationState = true;
+        }
+        else validationState = false;
+        break;
+      default:
+        if (
+          (taskName.trim() &&
+            taskName.length > 0 &&
+            taskNamePattern.test(taskName)) ||
+          (priority.trim() && priority.length > 0 && priorityPattern.test(priority))
+        ) {
+          validationState =  true;
+        }
+        else validationState = false;
+        break;
+    }
+    return validationState;
+}
+const createTodoItem = (taskName, priority, list, id) => {
   const row = document.createElement("tr");
   const idCell = document.createElement("td");
   const taskNameCell = document.createElement("td");
@@ -35,8 +39,9 @@ const createTodoItem = (taskName, priority, list, ID) => {
   const buttonCell = document.createElement("td");
   const editButton = document.createElement("button");
   const deleteButton = document.createElement("button");
-  row.setAttribute("id", ID);
-  idCell.innerHTML = ID;
+  const doneCheckbox = document.createElement("input");
+  row.setAttribute("id", id);
+  idCell.innerHTML = id;
   taskNameCell.innerHTML = taskName;
   priorityCell.innerHTML = priority;
   editButton.innerHTML = "Edit";
@@ -50,11 +55,29 @@ const createTodoItem = (taskName, priority, list, ID) => {
   deleteButton.classList.add("btn", "btn-danger");
   deleteButton.addEventListener("click", () => {
     row.remove();
-    list = removeTodoFromList(ID, list);
-    ID--;
+    list = list.filter((todo) => todo.ID !== id);
+    --ID;
+  });
+  doneCheckbox.setAttribute("type", "checkbox");
+  doneCheckbox.setAttribute("id", id);
+  doneCheckbox.classList.add("form-check-input");
+  doneCheckbox.style.marginTop = "3%";
+  doneCheckbox.addEventListener("change", () => {
+    const found = list.find((todo) => todo.ID === id);
+    if(found.status === 'done') {
+      found.status = 'not done';
+      editButton.removeAttribute('disabled');
+      taskNameCell.style.textDecoration = "none";
+    }
+    else{
+      found.status = 'done';
+      editButton.setAttribute('disabled', 'disabled');
+      taskNameCell.style.textDecoration = "line-through";
+    }
   });
   buttonCell.appendChild(editButton);
   buttonCell.appendChild(deleteButton);
+  buttonCell.appendChild(doneCheckbox);
   row.appendChild(idCell);
   row.appendChild(taskNameCell);
   row.appendChild(priorityCell);
@@ -67,10 +90,11 @@ const handelAddButton = (list, ID) => {
   const priority = document.getElementById("priority");
   const inputValue = input.value;
   const priorityValue = priority.value;
-  if (validate(inputValue, priorityValue)) {
+  if (validate(inputValue, priorityValue, true)) {
     const intPriority = parseInt(priorityValue);
-    list = addTodoToList([ID, inputValue, intPriority], list);
-    const row = createTodoItem(inputValue, intPriority, list, ID);
+    const task = new Task(inputValue, intPriority, ID);
+    list.push(task);
+    const row = createTodoItem(task.taskName, task.priority, list, task.ID);
     tasksList.appendChild(row);
     input.value = "";
     priority.value = "";
@@ -78,8 +102,6 @@ const handelAddButton = (list, ID) => {
     alert("Invalid input");
   }
 };
-
-
 
 const handleSendButton = (list) => {
   const rowToEdit = document.getElementById(localStorage.getItem("taskID"));
@@ -89,11 +111,13 @@ const handleSendButton = (list) => {
   const newPriority = document.getElementById("priority-text");
   const newTaskNameValue = newTaskName.value;
   const newPriorityValue = newPriority.value;
-  if (validateEdit(newTaskNameValue, newPriorityValue)) {
+  if (validate(newTaskNameValue, newPriorityValue, false)) {
     const intNewPriority = parseInt(newPriorityValue);
-    list = editTodo(ID, newTaskNameValue, intNewPriority, list);
-    if(newTaskNameValue) taskNameCell.innerHTML = newTaskNameValue;
-    if(intNewPriority) priorityCell.innerHTML = intNewPriority;
+    const foundElement = list.find(element => element.ID === parseInt(localStorage.getItem("taskID")));
+    foundElement.taskName = newTaskNameValue ?? foundElement.taskName;
+    foundElement.priority = intNewPriority ?? foundElement.priority;
+    taskNameCell.innerHTML = foundElement.taskName;
+    priorityCell.innerHTML = foundElement.priority;
     newTaskName.value = "";
     newPriority.value = "";
   } else {
@@ -101,47 +125,60 @@ const handleSendButton = (list) => {
   }
 };
 
-const sortTableByTaskName = (list) => {
-  const listSortedTaskName = list.sort((a, b) => {
-    return a.taskName.localeCompare(b.taskName);
-  });
+const handleDeleteAllButton = (list) => {
+  const inputElements = document.getElementsByTagName("input");
   const tableBody = document.getElementById("tasksList");
-  tableBody.innerHTML = "";
-  for (const todo of listSortedTaskName) {
-    const row = createTodoItem(todo.taskName, todo.priority, listSortedTaskName, todo.id);
-    tableBody.appendChild(row);
+  for(let i = 0; i < inputElements.length; i++) {
+    if(inputElements[i].type === "checkbox") {
+      if(inputElements[i].checked) {
+        tableBody.removeChild(inputElements[i].parentElement.parentElement);
+        list = list.filter((todo) => todo.ID !== parseInt(inputElements[i].id));
+        ID--;
+      }
+    }
   }
-};
+}
 
-const sortTableByPriority = (list) => {
-  const listSortedPriority = list.sort((a, b) => {
-    return a.priority - b.priority;
-  });
+
+const sortColumn = (list, column) => {
+  let listSorted=[]; 
+  switch (column) {
+    case 'name':
+      listSorted = list.sort((a, b) => {return a.taskName.localeCompare(b.taskName);});
+      break;
+    default:
+      listSorted = list.sort((a, b) => {return a.priority - b.priority;});
+      break;
+  }
   const tableBody = document.getElementById("tasksList");
   tableBody.innerHTML = "";
-  for (const todo of listSortedPriority) {
-    const row = createTodoItem(todo.taskName, todo.priority, listSortedPriority, todo.id);
+  for (const todo of listSorted) {
+    const row = createTodoItem(todo.taskName, todo.priority, listSorted, todo.ID);
     tableBody.appendChild(row);
   }
-};
+}
+
+
 
 const listOfTodos = new Array();
 const addButton = document.getElementById("add");
 const sendButton = document.getElementById("sendButton");
 const taskNameCol = document.getElementById("taskNameCol");
 const priorityCol = document.getElementById("priorityCol");
+const deleteAll = document.getElementById("clear");
 let ID = 0;
 addButton.addEventListener("click", function () {
   handelAddButton(listOfTodos, ++ID);
 });
-
-
 sendButton.addEventListener("click", function () {
   handleSendButton(listOfTodos);
 });
 taskNameCol.addEventListener("click", function () {
-  sortTableByTaskName(listOfTodos);
+  sortColumn(listOfTodos, 'name');
 });
 priorityCol.addEventListener("click", function () {
-  sortTableByPriority(listOfTodos);
+  sortColumn(listOfTodos, 'priority');
+});
+deleteAll.addEventListener("click", function () {
+  handleDeleteAllButton(listOfTodos);
 });
